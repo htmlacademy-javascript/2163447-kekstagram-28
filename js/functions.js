56e4bde4e036277676e094c6fffe7f8708d439c4
Anastasiia <soboanastasiya@yandex.ru>
2023-02-25 19:19:46 +0300

    встроенные функции

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+// проверка длины строки
+const lineLength = (string, number) => string.length <= number;
+lineLength ('', 20);
+
+// палиндром
+function palindrome(str) {
+  str = str.toLowerCase();
+  return str === str.split('').reverse().join('');
+}
+
+console.log(palindrome('топот'));
+console.log(palindrome('ДовОД'));
+console.log(palindrome('Кекс'));
+
+// Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
+const extractNumber = (string) => {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt(result, 10);
+}
+
+// Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины
+const myPadStart = (string, minLength, pad) => {
+  const actualPad = minLength - string.length;
+  if (actualPad <= 0) {
+    return string;
+  }
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
+}
