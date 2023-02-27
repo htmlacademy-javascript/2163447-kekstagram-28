67b78a5b443dfbf9e1e4b287e250314a627b83c5
Keks <keks@htmlacademy.ru>
2023-02-27 10:28:26 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,33 +1,23 @@
-// проверка длины строки
-const lineLength = (string, number) => string.length <= number;
-lineLength ('', 20);
+const checkStringLength = (string, number) => string.length <= number;
+checkStringLength('', 20);
 
-// палиндром
-function palindrome(str) {
-  str = str.toLowerCase();
-  return str === str.split('').reverse().join('');
-}
+const isPalindrome = (string) => {
+  string = string.toLowerCase().replaceAll(' ', '');
+  return string === string.split('').reverse().join('');
+};
 
-console.log(palindrome('топот'));
-console.log(palindrome('ДовОД'));
-console.log(palindrome('Кекс'));
+isPalindrome('топот');
 
-// Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
-const extractNumber = (string) => {
-  let result = '';
-  for (let i = 0; i < string.length; i++) {
-    if (!Number.isNaN(parseInt(string.at(i), 10))) {
-      result += string.at(i);
-    }
+const extractNumber = (string) =>
+  parseInt(String(string).replace(/\D/g, ''), 10);
+
+extractNumber('123ed');
+
+const createString = (string, minLength, pad) => {
+  while (minLength > string.length) {
+    string = pad.slice(0, minLength - string.length) + string;
   }
-  return parseInt(result, 10);
-}
+  return string;
+};
 
-// Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины
-const myPadStart = (string, minLength, pad) => {
-  const actualPad = minLength - string.length;
-  if (actualPad <= 0) {
-    return string;
-  }
-  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + string;
-}
+createString('1', 10, '3');
