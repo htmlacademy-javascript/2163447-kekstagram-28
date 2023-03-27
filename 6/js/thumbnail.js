551bc3539f4d8254af063a2dbc7c50eb35c2a495
Keks <keks@htmlacademy.ru>
2023-03-27 10:02:28 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/thumbnail.js
@@ -0,0 +1,22 @@
+import { getPictures } from './data.js';
+import { openBigPicture } from './big-picture.js';
+
+const thumbnailRendering = document.querySelector('#picture').content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+const data = getPictures();
+
+const createThumbnail = (item) => {
+  const thumbnail = thumbnailRendering.cloneNode(true);
+  thumbnail.querySelector('.picture__img').src = item.url;
+  thumbnail.querySelector('.picture__likes').textContent = item.likes;
+  thumbnail.querySelector('.picture__comments').textContent = item.comments.length;
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openBigPicture(item);
+  });
+  return thumbnail;
+};
+
+const renderThumbnails = () => data.forEach((item) => container.append(createThumbnail(item)));
+
+export { renderThumbnails };
